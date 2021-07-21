19132b99b324a16450e366bf7bf5f930fbd8ca70
keivanipchihagh <ipchi1380@gmail.com>
2021-07-21 13:17:44 +0430

    Initial setup
    - Added SASS compiler support
    - Configured static files
    - Configured templates
    - Setup for 'home' app

--- kerivanipchihagh/home/views.py
+++ kerivanipchihagh/home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    ''' Index view '''
+
+    return render(request, template_name = 'home/index.html', context = None)
--- kerivanipchihagh/kerivanipchihagh/settings.py
+++ kerivanipchihagh/kerivanipchihagh/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig',         # home app
+    'sass_processor',               # sass/scss compiler
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
+
+# Django Sass
+SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static')
